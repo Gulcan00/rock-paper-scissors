cdab423dd1251332b6f89a361a4d432efaca76e9
Gulcan00 <zipwire1@hotmail.com>
2023-03-09 11:58:44 +0200

    Implement playRound function

--- rpsGame.js
+++ rpsGame.js
@@ -3,3 +3,34 @@ function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 3);
     return choices[randNum];
 }
+
+function playRound(playerSelection, computerSelection) {
+    //Capitalize first letter of playerSelection
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+    if(playerSelection === 'Rock') {
+        if(computerSelection === 'Scissors') {
+            return 'You win! Rock beats Scissors';
+        } else if(computerSelection === 'Paper') {
+            return 'You lose! Paper beats Rock';
+        } else {
+            return `It's a tie!`;
+        }
+    } else if(playerSelection === 'Paper') {
+        if(computerSelection === 'Scissors') {
+            return 'You lose! Scissors beats Paper';
+        } else if(computerSelection === 'Rock') {
+            return 'You win! Paper beats Rock';
+        } else {
+            return `It's a tie!`;
+        }
+    } else {
+        if(computerSelection === 'Rock') {
+            return 'You lose! Rock beats Scissors';
+        } else if(computerSelection === 'Paper') {
+            return 'You win! Scissors beats Paper';
+        } else {
+            return `It's a tie!`;
+        }
+    }
+}
